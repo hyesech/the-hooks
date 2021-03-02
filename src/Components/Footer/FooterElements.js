1faa7b9cafdf26eefea4aa161662a92f80e0c1b7
hyesech <hyesech@gmail.com>
2021-03-02 16:46:26 +0900

    + update

--- src/Components/Footer/FooterElements.js
+++ src/Components/Footer/FooterElements.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const FooterContainer = styled.div`
-  background-color: #010606;
+  background-color: black;
 `;
 
 export const FooterWrap = styled.div`
